2dc5f82da7035aacb5d215ff9b8a1fe8d392bfec
mateusenmaarten <42380172+mateusenmaarten@users.noreply.github.com>
2020-11-02 12:20:33 +0100

    Added connectionString and basic DbSet with initial OnModelCreating tables

--- Roleplay/Data/ApplicationDbContext.cs
+++ Roleplay/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using PR5_Roleplay.Models;
 
 namespace Roleplay.Data
 {
@@ -11,6 +12,25 @@ namespace Roleplay.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Adventure> Adventures { get; set; }
+        public DbSet<AdventurePlayer> AdventurePlayers { get; set; }
+        public DbSet<Character> Characters { get; set; }
+        public DbSet<CharacterClass> CharacterClasses  { get; set; }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Session> Sessions { get; set; }
+        public DbSet<SessionPlayer> SessionPlayers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Adventure>().ToTable("Adventure");
+            builder.Entity<AdventurePlayer>().ToTable("AdventurePlayer");
+            builder.Entity<Character>().ToTable("Character");
+            builder.Entity<CharacterClass>().ToTable("CharacterClass");
+            builder.Entity<Player>().ToTable("Player");
+            builder.Entity<Session>().ToTable("Session");
+            builder.Entity<SessionPlayer>().ToTable("SessionPlayer");
         }
     }
 }
