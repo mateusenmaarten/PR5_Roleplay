5bc83655d21e7385b7dbdc4ade79378f9e90f2ee
Mateusen Maarten <mateusenmaarten@hotmail.com>
2020-12-06 15:30:22 +0100

    Seeded character class table

--- Roleplay/Data/ApplicationDbContext.cs
+++ Roleplay/Data/ApplicationDbContext.cs
@@ -48,6 +48,85 @@ namespace Roleplay.Data
             builder.Entity<CharacterClass>().Property(p => p.CharacterClassName).IsRequired();
             builder.Entity<CharacterClass>().Property(p => p.CharacterClassDescription).IsRequired();
             builder.Entity<CharacterClass>().Property(p => p.CharacterClassIcon).IsRequired();
+            builder.Entity<CharacterClass>().HasData(
+                new CharacterClass
+                {
+                    CharacterClassID = 1,
+                    CharacterClassName = "Barbarian",
+                    CharacterClassIcon = "fas fa-dumbbell",
+                    CharacterClassDescription = "Big, strong and reckless"
+                },
+                new CharacterClass
+                {
+                    CharacterClassID = 2,
+                    CharacterClassName = "Bard",
+                    CharacterClassIcon = "fas fa-guitar",
+                    CharacterClassDescription = "Melodic and charismatic to the core"
+                },
+                new CharacterClass
+                {
+                    CharacterClassID = 3,
+                    CharacterClassName = "Cleric",
+                    CharacterClassIcon = "fas fa-plus-square",
+                    CharacterClassDescription = "Healing wonder"
+                },
+                new CharacterClass
+                {
+                    CharacterClassID = 4,
+                    CharacterClassName = "Fighter",
+                    CharacterClassIcon = "fas fa-fist-raised",
+                    CharacterClassDescription = "Weapons Master"
+                },
+                new CharacterClass
+                {
+                    CharacterClassID = 5,
+                    CharacterClassName = "Monk",
+                    CharacterClassIcon = "fas fa-praying-hands",
+                    CharacterClassDescription = "religious with a punch"
+                },
+                new CharacterClass
+                {
+                    CharacterClassID = 6,
+                    CharacterClassName = "Paladin",
+                    CharacterClassIcon = "fas fa-jedi",
+                    CharacterClassDescription = "There can be only one divine, mine"
+                },
+                new CharacterClass
+                {
+                    CharacterClassID = 7,
+                    CharacterClassName = "Ranger",
+                    CharacterClassIcon = "fas fa-bullseye",
+                    CharacterClassDescription = "Expert marksman"
+                },
+                new CharacterClass
+                 {
+                     CharacterClassID = 8,
+                     CharacterClassName = "Druid",
+                     CharacterClassIcon = "fas fa-paw",
+                     CharacterClassDescription = "In tune with nature and it's animals"
+                 },
+                new CharacterClass
+                  {
+                      CharacterClassID = 9,
+                      CharacterClassName = "Rogue",
+                      CharacterClassIcon = "fas fa-user-ninja",
+                      CharacterClassDescription = "Sneaky thieving raskal"
+                  },
+                new CharacterClass
+                  {
+                      CharacterClassID = 10,
+                      CharacterClassName = "Sorcerer",
+                      CharacterClassIcon = "fas fa-hand-sparkles",
+                      CharacterClassDescription = "Uncontrollable power at it's fingertips"
+                  },
+                new CharacterClass
+                   {
+                       CharacterClassID = 11,
+                       CharacterClassName = "Wizard",
+                       CharacterClassIcon = "fas fa-hat-wizard",
+                       CharacterClassDescription = "Spelcaster that can keep it's cool"
+                   }
+            );
 
             //Player
             builder.Entity<Player>().ToTable("Player");
---
+++ Roleplay/Migrations/20201206142808_seed characterclasses.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Roleplay.Data.Migrations
+{
+    public partial class seedcharacterclasses : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                schema: "PR5",
+                table: "CharacterClass",
+                columns: new[] { "CharacterClassID", "CharacterClassDescription", "CharacterClassIcon", "CharacterClassName" },
+                values: new object[,]
+                {
+                    { 1, "Big, strong and reckless", "fas fa-dumbbell", "Barbarian" },
+                    { 2, "Melodic and charismatic to the core", "fas fa-guitar", "Bard" },
+                    { 3, "Healing wonder", "fas fa-plus-square", "Cleric" },
+                    { 4, "Weapons Master", "fas fa-fist-raised", "Fighter" },
+                    { 5, "religious with a punch", "fas fa-praying-hands", "Monk" },
+                    { 6, "There can be only one divine, mine", "fas fa-jedi", "Paladin" },
+                    { 7, "Expert marksman", "fas fa-bullseye", "Ranger" },
+                    { 8, "In tune with nature and it's animals", "fas fa-paw", "Druid" },
+                    { 9, "Sneaky thieving raskal", "fas fa-user-ninja", "Rogue" },
+                    { 10, "Uncontrollable power at it's fingertips", "fas fa-hand-sparkles", "Sorcerer" },
+                    { 11, "Spelcaster that can keep it's cool", "fas fa-hat-wizard", "Wizard" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "PR5",
+                table: "CharacterClass",
+                keyColumn: "CharacterClassID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                schema: "PR5",
+                table: "CharacterClass",
+                keyColumn: "CharacterClassID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                schema: "PR5",
+                table: "CharacterClass",
+                keyColumn: "CharacterClassID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                schema: "PR5",
+                table: "CharacterClass",
+                keyColumn: "CharacterClassID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                schema: "PR5",
+                table: "CharacterClass",
+                keyColumn: "CharacterClassID",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                schema: "PR5",
+                table: "CharacterClass",
+                keyColumn: "CharacterClassID",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                schema: "PR5",
+                table: "CharacterClass",
+                keyColumn: "CharacterClassID",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                schema: "PR5",
+                table: "CharacterClass",
+                keyColumn: "CharacterClassID",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                schema: "PR5",
+                table: "CharacterClass",
+                keyColumn: "CharacterClassID",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                schema: "PR5",
+                table: "CharacterClass",
+                keyColumn: "CharacterClassID",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                schema: "PR5",
+                table: "CharacterClass",
+                keyColumn: "CharacterClassID",
+                keyValue: 11);
+        }
+    }
+}
