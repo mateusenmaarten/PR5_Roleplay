2288898a3346bffadb70092acbec5593021ac893
Mateusen Maarten <mateusenmaarten@hotmail.com>
2020-12-12 16:35:41 +0100

    added customData to registerForm

--- Roleplay/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Roleplay/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -14,6 +14,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
 using Roleplay.Areas.Identity.Data;
+using Roleplay.Data;
 
 namespace Roleplay.Areas.Identity.Pages.Account
 {
@@ -24,17 +25,20 @@ namespace Roleplay.Areas.Identity.Pages.Account
         private readonly UserManager<CustomUser> _userManager;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
+        private readonly ApplicationDbContext _context;
 
         public RegisterModel(
             UserManager<CustomUser> userManager,
             SignInManager<CustomUser> signInManager,
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            IEmailSender emailSender,
+            ApplicationDbContext context)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
             _emailSender = emailSender;
+            _context = context;
         }
 
         [BindProperty]
@@ -46,6 +50,20 @@ namespace Roleplay.Areas.Identity.Pages.Account
 
         public class InputModel
         {
+            [DataType(DataType.Text)]
+            [Display(Name = "FirstName")]
+            public string FirstName { get; set; }
+            [DataType(DataType.Text)]
+            [Display(Name = "LastName")]
+            public string LastName { get; set; }
+            [Required]
+            [DataType(DataType.Text)]
+            [Display(Name = "UserName")]
+            public string UserName { get; set; }
+            [Required]
+            [Display(Name = "IsGameMaster")]
+            public bool IsGameMaster { get; set; }
+
             [Required]
             [EmailAddress]
             [Display(Name = "Email")]
