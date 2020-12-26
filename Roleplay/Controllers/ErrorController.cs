79fdc31f90bbcd71294fcdf361d08b5f025e132c
Mateusen Maarten <mateusenmaarten@hotmail.com>
2020-12-26 11:32:31 +0100

    custom 404 page

---
+++ Roleplay/Controllers/ErrorController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Roleplay.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("Error/{statusCode}")]
+        public IActionResult HttpStatusCodeHandler(int statusCode)
+        {
+            switch (statusCode)
+            {
+                case 404:
+                    ViewBag.ErrorMessage = "You have been wandering for days...";
+                    break;
+            }
+            return View("NotFound");
+        }
+    }
+}
--- Roleplay/Startup.cs
+++ Roleplay/Startup.cs
@@ -64,6 +64,9 @@ namespace Roleplay
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
         {
+             
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -71,9 +74,7 @@ namespace Roleplay
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseStatusCodePagesWithRedirects("/Error/{0}");
             }
 
             CultureInfo cultureInfoDutchBelgium = new CultureInfo("nl-BE");
